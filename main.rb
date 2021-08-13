b338d87bdb6a5ace71ccbab6f3cd23f03ab3bed2
Jeremiah Tabb <55849254+jtabb1@users.noreply.github.com>
2021-08-13 02:22:45 +0000

    Solved problem

--- main.rb
+++ main.rb
@@ -1,6 +1,18 @@
-x_test = [0, 4, 4, 6, 7, 1, 3]
-tgt = 10
-
 def two_sum(array, target)
+  memo = {}
 
+  array.each_with_index do |int,ix|
+    solved = target - int
+
+    if memo.key?(solved)
+      return [ix, memo[solved]]
+    end
+
+    memo[int] = ix
+  end
 end
+
+ary01 = [0, 4, 7, 6, 2, 1, 3]
+tgt01a = 10
+
+puts two_sum(ary01,tgt01a).inspect
